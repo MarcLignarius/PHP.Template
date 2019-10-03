2d760c775fda5bff62eb7f9450532eed3a575075
Marc Davies <marcdaviesriot@gmail.com>
2019-10-03 14:52:32 -0700

    add container

--- breadcrumb.php
+++ breadcrumb.php
@@ -1,11 +1,14 @@
 <!-- Page Heading/Breadcrumbs -->
-<h1 class="mt-4 mb-3">Page Heading
-      <small>Page Subheading</small>
+<div class="container">
+    <h1 class="mt-4 mb-3">Page Heading
+        <small>Page Subheading</small>
     </h1>
 
     <ol class="breadcrumb">
-      <li class="breadcrumb-item">
-        <a href="index.php">Home</a>
-      </li>
-      <li class="breadcrumb-item active">Page Name</li>
+        <li class="breadcrumb-item">
+            <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">Page Name</li>
     </ol>
+</div>  
+    
