fad6677271c4f3be7a5b8a1fa33a31f5bafc8256
Marc Davies <marcdaviesriot@gmail.com>
2019-10-03 16:13:41 -0700

    add php echos for page title

--- breadcrumb.php
+++ breadcrumb.php
@@ -1,6 +1,6 @@
 <!-- Page Heading/Breadcrumbs -->
 <div class="container">
-    <h1 class="mt-4 mb-3">Page Heading
+    <h1 class="mt-4 mb-3"><?php echo $page_title; ?>
         <small>Page Subheading</small>
     </h1>
 
@@ -8,7 +8,7 @@
         <li class="breadcrumb-item">
             <a href="index.php">Home</a>
         </li>
-        <li class="breadcrumb-item active">Page Name</li>
+        <li class="breadcrumb-item active"><?php echo $page_title; ?></li>
     </ol>
 </div>  
     
