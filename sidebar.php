3084f1d1bbcdc14809d758c1d1379e3652f187e2
Marc Davies <marcdaviesriot@gmail.com>
2019-10-03 16:44:14 -0700

    refactor, rename and change links to sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,45 @@
+<?php $page_title = "Sidebar"; ?>
+
+<?php include("header.php"); ?>
+<?php include("navigation.php"); ?>
+<?php include("breadcrumb.php"); ?>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Content Row -->
+    <div class="row">
+      <!-- Sidebar Column -->
+      <div class="col-lg-3 mb-4">
+        <div class="list-group">
+          <a href="index.php" class="list-group-item">Home</a>
+          <a href="about.php" class="list-group-item">About</a>
+          <a href="services.html" class="list-group-item">Services</a>
+          <a href="contact.php" class="list-group-item">Contact</a>
+          <a href="portfolio-1-col.php" class="list-group-item">1 Column Portfolio</a>
+          <a href="portfolio-2-col.php" class="list-group-item">2 Column Portfolio</a>
+          <a href="portfolio-3-col.php" class="list-group-item">3 Column Portfolio</a>
+          <a href="portfolio-4-col.php" class="list-group-item">4 Column Portfolio</a>
+          <a href="portfolio-item.php" class="list-group-item">Single Portfolio Item</a>
+          <a href="blog-home-1.php" class="list-group-item">Blog Home 1</a>
+          <a href="blog-home-2.php" class="list-group-item">Blog Home 2</a>
+          <a href="blog-post.php" class="list-group-item">Blog Post</a>
+          <a href="sidebar.php" class="list-group-item active">Sidebar Page</a>
+          <a href="faq.php" class="list-group-item">FAQ</a>
+          <a href="404.php" class="list-group-item">404</a>
+          <a href="pricing.php" class="list-group-item">Pricing Table</a>
+        </div>
+      </div>
+      <!-- Content Column -->
+      <div class="col-lg-9 mb-4">
+        <h2>Section Heading</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, et temporibus, facere perferendis veniam beatae non debitis, numquam blanditiis necessitatibus vel mollitia dolorum laudantium, voluptate dolores iure maxime ducimus fugit.</p>
+      </div>
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <?php include("call-to-action.php"); ?>
+<?php include("footer.php"); ?>
