1abf1abbde709bb82d95e62bd3199c7b16f39266
Marc Davies <marcdaviesriot@gmail.com>
2019-10-03 15:53:59 -0700

    add hr to this component

--- call-to-action.php
+++ call-to-action.php
@@ -1,5 +1,6 @@
 <!-- Call to Action Section -->
 <div class="container">
+<hr>
     <div class="row mb-4">
         <div class="col-md-8">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
