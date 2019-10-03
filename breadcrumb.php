eaf6bbeeac2d82a89ff897f50a8ef4d8a3145740
Marc Davies <marcdaviesriot@gmail.com>
2019-10-03 14:33:59 -0700

    add breadcrumb.php

---
+++ breadcrumb.php
@@ -0,0 +1,11 @@
+<!-- Page Heading/Breadcrumbs -->
+<h1 class="mt-4 mb-3">Page Heading
+      <small>Page Subheading</small>
+    </h1>
+
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">
+        <a href="index.html">Home</a>
+      </li>
+      <li class="breadcrumb-item active">Page Name</li>
+    </ol>
